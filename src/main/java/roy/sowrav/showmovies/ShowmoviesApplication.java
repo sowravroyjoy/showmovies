d0857ac4a638e51b674b2da4f98c72fbb37e2231
sowravroyjoy <sowravroyjoy@gmail.com>
2024-02-20 12:39:41 -0500

    fix the cross origin problem

--- src/main/java/roy/sowrav/showmovies/ShowmoviesApplication.java
+++ src/main/java/roy/sowrav/showmovies/ShowmoviesApplication.java
@@ -2,6 +2,9 @@ package roy.sowrav.showmovies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -12,5 +15,14 @@ public class ShowmoviesApplication {
 		SpringApplication.run(ShowmoviesApplication.class, args);
 	}
 
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:3000");
+//			}
+//		};
+//	}
 
 }
